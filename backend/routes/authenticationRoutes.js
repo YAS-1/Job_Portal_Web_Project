bd843d541bb8cd42fc8f996cbcbedd55b841159b
rebecca26-bit <alindarebecca5@gmail.com>
2025-03-17 23:47:36 +0300

    Authenticator routes

---
+++ backend/routes/authenticationRoutes.js
@@ -0,0 +1,28 @@
+//This is part of Milestone 2 giving is core functionality through User Authentication
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/users.model");
+const router = express.Router();
+
+
+router.post("/register", async(req, res) => {
+    const { name, email, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword, role });
+    await user.save();
+    res.status(201).json({ message: "User Registered" });
+});
+
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(400).json({ message: "Invalid Credentials"});
+    }
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {expiresIn: "1h"});
+    res.json({ token });
+});
+
+module.exports = router;
